4c9c3f500e3d8ba707b2d77b59bf0fd05d2a2405
sum-bocfob <43840414+sum-bocfob@users.noreply.github.com>
2025-04-05 09:03:50 +0900

    予約フォームの基本的な遷移作成

--- js/main.js
+++ js/main.js
@@ -36,3 +36,29 @@ const elms = document.querySelectorAll(".c-menu");
 elms.forEach((elm) => {
     observer.observe(elm);
 });
+
+// *予約確認モーダル ////////////////////////////////////////////////
+const openBtn = document.querySelector(".js-open-modal");
+const dialog = document.querySelector(".p-dialog");
+openBtn.addEventListener("click", function () {
+    dialog.showModal();
+});
+
+// 予約確定
+const confirmBtn = document.querySelector(".p-dialog__confirm");
+const dialogGotcha = document.querySelector(".p-dialog-gotcha");
+confirmBtn.addEventListener("click", function () {
+    dialog.close();
+    dialogGotcha.show();
+});
+// 予約確定モーダルを閉じる
+const closeGotchaBtn = document.querySelector(".p-dialog-gotcha__close");
+closeGotchaBtn.addEventListener("click", function () {
+    dialogGotcha.close();
+});
+
+// モーダル内キャンセル
+const cancelBtn = document.querySelector(".p-dialog__cancel");
+cancelBtn.addEventListener("click", function () {
+    dialog.close();
+});
